20300fe6f2d0fbb87acfc7d2723079352c6f1bfa
Tong Du <tong.du@roche.com>
2018-04-25 17:06:37 -0700

    initial update

---
+++ add-two-numbers/src/Main.java
@@ -0,0 +1,29 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        ListNode a = new ListNode(2, 4, 3);
+        ListNode b = new ListNode(5, 6, 4);
+        System.out.printf("%s + %s = %s%n", a, b, addTwoNumbers(a, b));
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.data : 0;
+            int y = (q != null) ? q.data : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+}
